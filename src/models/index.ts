2d9495cc432b1c943d1c2f3bb4e4a78b0d09f2a8
dni ⚡ <office@dnilabs.com>
2023-07-04 13:06:03 +0200

    update client

---
+++ src/models/index.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ConversionData';
+export * from './CreateExtension';
+export * from './CreateInvoiceData';
+export * from './CreateLNURLData';
+export * from './CreateLnurlAuth';
+export * from './CreateTopup';
+export * from './DecodePayment';
+export * from './EditableSettings';
+export * from './HTTPValidationError';
+export * from './LocationInner';
+export * from './Page';
+export * from './Payment';
+export * from './ValidationError';
